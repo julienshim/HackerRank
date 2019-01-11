0876265577c179dd889df37cfc2eff8a07035a6d
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-01-10 20:57:25 -0800

    day 1 data types swift

---
+++ 30-Days-Of-Code/swift/Day 1: Data Types.swift
@@ -0,0 +1,18 @@
+var i = 4
+var d = 4.0
+var s = "HackerRank "
+// Declare second integer, double, and String variables.
+var ib: Int
+var db: Double
+var sb: String
+// Read and save an integer, double, and String to your variables.
+ib = Int(readLine()!)!
+db = Double(readLine()!)!
+sb = readLine()!
+// Print the sum of both integer variables on a new line.
+print(i + ib)
+// Print the sum of the double variables on a new line.
+print(d + db)
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+print(s + sb)
