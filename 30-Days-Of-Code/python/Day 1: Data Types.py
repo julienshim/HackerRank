a791d6c070483f2139ee139ae4706f0364dd4640
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-01-10 20:31:31 -0800

    day 1 data types  py

---
+++ 30-Days-Of-Code/python/Day 1: Data Types.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+ib = 0
+db = 0.0
+sb = ""
+# Read and save an integer, double, and String to your variables.
+ib = int(input())
+db = float(input())
+sb = input()
+# Print the sum of both integer variables on a new line.
+print(i + ib)
+# Print the sum of the double variables on a new line.
+print(d + db)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + sb)
