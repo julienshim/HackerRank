01ee7ef1b755146560af6395713e30655ba98f71
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-01-13 19:11:50 -0800

    day 4 swift version

---
+++ 30-Days-Of-Code/swift/Day 4: Class vs Instance.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+class Person {
+    var age: Int = 0
+
+    init(initialAge: Int) {
+        // Add some more code to run some checks on initialAge
+        if initialAge < 0 {
+            self.age = 0
+            print("Age is not valid, setting age to 0.")
+        } else {
+            self.age = initialAge
+        }
+    }
+
+    func amIOld() {
+        // Do some computations in here and print out the correct statement to the console
+        if self.age < 13 {
+            print("You are young.")
+        } else if self.age >= 13 && self.age < 18 {
+            print("You are a teenager.")
+        } else {
+            print("You are old.")
+        }
+    }
+
+    func yearPasses() {
+        // Increment the age of the person in here
+        self.age = self.age + 1
+    }
+}
