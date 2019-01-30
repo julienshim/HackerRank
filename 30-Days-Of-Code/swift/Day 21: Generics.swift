7db4379e9f870383eaa3fa7bebb22b0fafa71317
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-01-30 12:27:49 -0800

    Update Day 21: Generics.swift

--- 30-Days-Of-Code/swift/Day 21: Generics.swift
+++ 30-Days-Of-Code/swift/Day 21: Generics.swift
@@ -1,5 +1,14 @@
-func printArray(array: [T]){
-    for i in array {
-        print(i)
+struct Printer<T> {
+	/**
+	*    Name: printArray
+	*    Print each element of the generic array on a new line. Do not return anything.
+	*    @param A generic array
+	**/
+	
+	// Write your code here
+    func printArray(array: [T]){
+        for i in array {
+            print(i)
+        }
     }
 }
