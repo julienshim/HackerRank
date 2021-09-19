44d92238833ba5e3d0ec188f0abd8a4cb9ee3632
julienshim <25723428+julienshim@users.noreply.github.com>
2021-09-18 23:03:31 -0700

    cap pass

--- Practice/Python/Strings/Capitalize-.py
+++ Practice/Python/Strings/Capitalize-.py
@@ -1,5 +1,3 @@
 def solve(s):
-    [first, last] = s.split(' ')
-    def capitalize(n):
-        return n[0].upper() + n[1:]
-    return ' '.join([capitalize(first), capitalize(last)])
+    sArr = s.split(' ')
+    return ' '.join(map(str.capitalize, sArr))
